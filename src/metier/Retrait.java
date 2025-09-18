765065738a5af498a7244c27bf2bb4db81f59ea7
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-09-18 13:58:49 +0100

    Add Retrait class to manage withdrawal operations

---
+++ src/metier/Retrait.java
@@ -0,0 +1,28 @@
+package metier;
+
+public class Retrait extends Operation {
+	
+	
+	private String destination;
+	
+	public Retrait(double montant,String destination)
+	{
+		super(montant);
+		this.destination=destination;
+		
+	}
+	
+	
+	public String getdestination()
+	{
+		return destination;
+	}
+	
+	public void setdestination()
+	{
+		this.destination =destination;
+	}
+	
+	
+    
+}
