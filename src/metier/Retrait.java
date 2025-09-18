b14942af92348b98d2d2be1643e05a52c282c5e0
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-09-18 15:09:34 +0100

    Fix setdestination method signature and improve afficherDetails formatting in Retrait class

--- src/metier/Retrait.java
+++ src/metier/Retrait.java
@@ -18,15 +18,21 @@ public class Retrait extends Operation {
 		return destination;
 	}
 	
-	public void setdestination()
+	public void setdestination(String destination)
 	{
 		this.destination = destination;
 	}
 	
 	
 	  @Override
-	    public void afficherDetails() {
-		  
+	public void afficherDetails()
+	  {
+		    System.out.println("#==== Détails Retrait ====#");
+		    System.out.println("Numéro      : " + getNumero());
+		    System.out.println("Date        : " + getDate());
+		    System.out.println("Montant     : " + getMontant());
+		    System.out.println("Destination : " + destination);
+		    System.out.println("#=========================#"); 
 	  }
 	
 	
