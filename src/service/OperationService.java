7f962681e50afbe8ca13800dc4d1ba22cda77779
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-09-19 14:35:53 +0100

    Implement withdrawal and transfer functionalities with enhanced error handling in OperationService

--- src/service/OperationService.java
+++ src/service/OperationService.java
@@ -4,6 +4,8 @@ import metier.Operation;
 import metier.Retrait;
 import metier.Versement;
 import metier.Compte;
+import metier.CompteEpargne;
+import metier.CompteCourant;
 
 
 
@@ -14,5 +16,66 @@ public class OperationService
         compte.verser(montant, source);
         System.out.println("Versement de " + montant + " effectué sur le compte " + compte.getCode());
     }
+    
+    public void effectuerRetrait(Compte compte, double montant, String destination)
+    {
+    	boolean retraitAutorise = false;
+    	if(compte instanceof CompteCourant)
+    	{
+    		CompteCourant compteCourant = (CompteCourant) compte;
+    		retraitAutorise = (compteCourant.getSolde() + compteCourant.getDecouvert() >= montant);
+    	}else
+    	{
+    		retraitAutorise = (compte.getSolde() >= montant);
+    	}
+    	if(retraitAutorise)
+    	{
+    		compte.retirer(montant);
+    		Retrait retrait = new Retrait(montant,destination);
+    		compte.getListeOperations().add(retrait);
+            System.out.println("Retrait de " + montant + " effectué vers " + destination + " sur le compte " + compte.getCode());
+
+    		
+    	}else
+    	{
+            System.out.println("Retrait impossible ! Solde insuffisant.");
+            if (compte instanceof CompteCourant) {
+                CompteCourant compteCourant = (CompteCourant) compte;
+                System.out.println("Solde actuel : " + compteCourant.getSolde() + ", découvert autorisé : " + compteCourant.getDecouvert());
+            } else {
+                System.out.println("Solde actuel : " + compte.getSolde());
+            }
+    	}
+    }
+    
+    
+    public void effectuerVirement(Compte compteSource, Compte compteDest, double montant) {
+        boolean retraitAutorise = false;
+
+        
+        if (compteSource instanceof CompteCourant) {
+            CompteCourant compteCourant = (CompteCourant) compteSource;
+            retraitAutorise = (compteCourant.getSolde() + compteCourant.getDecouvert() >= montant);
+        } else {
+            retraitAutorise = (compteSource.getSolde() >= montant);
+        }
+
+        if (retraitAutorise) {
+            
+            compteSource.retirer(montant);
+            Retrait retrait = new Retrait(montant, "Virement vers " + compteDest.getCode());
+            compteSource.getListeOperations().add(retrait);
+
+            
+            compteDest.verser(montant, "Virement reçu de " + compteSource.getCode());
+            Versement versement = new Versement(montant, "Virement de " + compteSource.getCode());
+            compteDest.getListeOperations().add(versement);
+
+            System.out.println("Virement de " + montant + " effectué de " + compteSource.getCode() + " vers " + compteDest.getCode());
+        } else {
+            System.out.println("Virement impossible ! Solde insuffisant sur le compte source.");
+        }
+    }
+
     
 }
